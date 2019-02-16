418bdb7e94f620d811a38c55d68250e993c1eb4e
uriegel <uriegel@hotmail.de>
2019-02-16 16:09:50 +0100

    Add project files.

---
+++ FSharpTools/EnumerableExtensions.fs
@@ -0,0 +1,28 @@
+﻿module EnumerableExtensions
+
+open System
+open System.Collections
+open System.Collections.Generic
+
+let castEnumerator<'U> (enumerator: IEnumerator) = {
+    new IEnumerator<'U> with
+        member x.Current with get() = enumerator.Current :?> 'U
+    interface IEnumerator with
+        member x.Current with get() = enumerator.Current
+        member x.MoveNext() = enumerator.MoveNext()
+        member x.Reset() = enumerator.Reset()
+    interface IDisposable with
+        member x.Dispose() = ()
+}      
+
+let makeSeq enumerator = {
+    new IEnumerable<'U> with
+        member x.GetEnumerator() = enumerator
+    interface IEnumerable with
+        member x.GetEnumerator() = 
+            (enumerator :> IEnumerator)
+}
+
+
+
+
---
+++ FSharpTools/Json.fs
@@ -0,0 +1,14 @@
+﻿module Json
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+
+let private defaultSettings = new JsonSerializerSettings(ContractResolver = CamelCasePropertyNamesContractResolver(),
+                                                //defaultSettings.TypeNameHandling = TypeNameHandling.All
+                                                DefaultValueHandling = DefaultValueHandling.Ignore)
+                                                
+let serialize obj = JsonConvert.SerializeObject (obj, defaultSettings)
+
+
+
+

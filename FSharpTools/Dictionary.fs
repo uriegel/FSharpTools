457c6734b2a44a0c08d99df76a3bc46353fa3477
uriegel <uriegel@hotmail.de>
2019-03-17 13:54:30 +0100

    Dictionary.Dict, Json.serializeWithOptions

---
+++ FSharpTools/Dictionary.fs
@@ -0,0 +1,21 @@
+﻿module Dictionary
+
+open System.Collections.Generic
+
+/// <summary>
+/// Converts a sequence of KeyValuePairs to a Dictionary.
+/// The key uses an IEqualityComparer
+/// </summary>
+/// <param name="equalityComparer">EqualityComparer used for comparing keys</param>
+/// <param name="keyValuePairs">Sequence of KeyValuePairs to convert to dictionary</param>
+/// <returns>The created Dictionary</returns>
+let Dict<'k,'v> (equalityComparer: IEqualityComparer<'k>) (keyValuePairs: seq<'k*'v>) = 
+    let result = Dictionary<'k,'v>(equalityComparer)
+    keyValuePairs
+    |> Seq.iter (fun (k, v) -> result.[k] <- v)
+    result :> IDictionary<'k,'v>
+
+
+
+
+
--- FSharpTools/Json.fs
+++ FSharpTools/Json.fs
@@ -1,5 +1,9 @@
 ﻿module Json
 
+open System
+
+open Microsoft.FSharp.Reflection
+
 open Newtonsoft.Json
 open Newtonsoft.Json.Serialization
 
@@ -14,6 +18,45 @@ let private defaultSettings = new JsonSerializerSettings(ContractResolver = Came
 /// <returns>JSON serialized object as string.</returns>
 let serialize obj = JsonConvert.SerializeObject (obj, defaultSettings)
 
+type OptionConverter() =
+    inherit JsonConverter()
+    
+    override x.CanConvert(objectType) = 
+        objectType.IsGenericType && objectType.GetGenericTypeDefinition() = typedefof<option<_>>
+
+    override x.WriteJson(writer, value, serializer) =
+        let value = 
+            match value with 
+            | null -> null
+            | _ -> 
+                let _, fields = FSharpValue.GetUnionFields(value, value.GetType())
+                fields.[0]  
+        serializer.Serialize(writer, value)
+
+    override x.ReadJson(reader, objectType, existingValue, serializer) =        
+        let innerType = objectType.GetGenericArguments().[0]
+        let innerType = 
+            if innerType.IsValueType then (typedefof<Nullable<_>>).MakeGenericType([|innerType|])
+            else innerType        
+        let value = serializer.Deserialize(reader, innerType)
+        let cases = FSharpType.GetUnionCases(objectType)
+        if value = null then 
+            FSharpValue.MakeUnion(cases.[0], [||])
+        else 
+            FSharpValue.MakeUnion(cases.[1], [|value|])
+
+let private defaultSettingsWithOptions =
+    new JsonSerializerSettings(ContractResolver = CamelCasePropertyNamesContractResolver(),
+                                Converters = [| OptionConverter() |], 
+                                DefaultValueHandling = DefaultValueHandling.Ignore)
+
+/// <summary>Serializing an object to JSON
+/// Options are supported
+/// Default value handling is no to emit default values
+/// </summary>
+/// <param name="obj">The object to serialize.</param>
+/// <returns>JSON serialized object as string.</returns>
+let serializeWithOptions obj = JsonConvert.SerializeObject (obj, defaultSettingsWithOptions)
 
 
 

8488840a244fe8b85ee8354174b180ed0dcdbe14
Uwe Riegel <uriegel@hotmail.de>
2020-03-15 07:46:02 +0100

    Strings extended

---
+++ FSharpTools/ActivePatterns.fs
@@ -0,0 +1,10 @@
+module ActivePatterns
+open System
+
+let (|Int|_|) (str: string option) =
+    match str with
+    | Some str -> 
+        match System.Int32.TryParse str with
+        | true,int -> Some int
+        | _ -> None
+    | _ -> None        
--- FSharpTools/String.fs
+++ FSharpTools/String.fs
@@ -127,3 +127,58 @@ let substring2 pos length (str: string) =
 
 let join (chr: char) (strs: string seq) = String.Join (chr, strs)
 
+let startsWith (testStr: string) (str: string) =
+    if not (isNull testStr) && not (isNull str) then
+        str.StartsWith testStr
+    else
+        false
+
+let joinStr (sep: string) (strs: string seq) = String.Join (sep, strs)
+
+let replace (a: string) (b: string) (str: string) =
+    if not (isNull str) then
+        str.Replace (a, b)
+    else
+        ""
+
+let replaceChar (a: char) (b: char) (str: string) =
+    if not (isNull str) then
+        str.Replace (a, b)
+    else
+        ""
+
+let padRight totalWidth (padChr: char) (str: string) =
+    if not (isNull str) then
+        str.PadRight (totalWidth, padChr)
+    else
+        ""
+
+let length (str: string) =
+    if not (isNull str) then
+        str.Length 
+    else
+        0
+
+let toUpper (str: string) =
+    if not (isNull str) then
+        str.ToUpper ()
+    else
+        ""
+
+let toLower (str: string) =
+    if not (isNull str) then
+        str.ToLower ()
+    else
+        ""
+let toUpperInvariant (str: string) =
+    if not (isNull str) then
+        str.ToUpperInvariant ()
+    else
+        ""
+
+let toLowerInvariant (str: string) =
+    if not (isNull str) then
+        str.ToLowerInvariant ()
+    else
+        ""
+

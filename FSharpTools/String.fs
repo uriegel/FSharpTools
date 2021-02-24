1010e243636a7186e87eff6040574ac8dcc197d9
Uwe Riegel <uriegel@hotmail.de>
2021-02-24 07:24:32 +0100

    String.split and String.splitMulti

--- FSharpTools/String.fs
+++ FSharpTools/String.fs
@@ -53,6 +53,32 @@ module String =
     /// Splits a string into parts, separator is one char
     /// If the string is null, an emtpy array is returned
     /// </summary>
+    /// <param name="sep">The separator</param>
+    /// <param name="str">String to be splitted</param>
+    /// <returns>The trimmed string</returns>
+    let split (sep: string) (str: string) =
+        match isNull str, isNull sep with
+        | false, false -> str.Split ([|sep|], StringSplitOptions.RemoveEmptyEntries)
+        | false, true -> [|str|]
+        |_ -> [||]
+
+    /// <summary>
+    /// Splits a string into parts, separator is one char
+    /// If the string is null, an emtpy array is returned
+    /// </summary>
+    /// <param name="seps">The separators</param>
+    /// <param name="str">String to be splitted</param>
+    /// <returns>The trimmed string</returns>
+    let splitMulti (seps: string[]) (str: string) =
+        match isNull str, isNull seps with
+        | false, false -> str.Split (seps, StringSplitOptions.RemoveEmptyEntries)
+        | false, true -> [|str|]
+        |_ -> [||]
+
+    /// <summary>
+    /// Splits a string into parts, separator is one char
+    /// If the string is null, an emtpy array is returned
+    /// </summary>
     /// <param name="chr">The separator</param>
     /// <param name="str">String to be splitted</param>
     /// <returns>The trimmed string</returns>

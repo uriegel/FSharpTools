b3a3fbcf6dc76bb7eea1cee491db2bb6afa814d2
Uwe Riegel <uriegel@hotmail.de>
2022-10-15 09:35:13 +0200

    memoize with resetter

--- FSharpTools/Functional.fs
+++ FSharpTools/Functional.fs
@@ -1,5 +1,23 @@
 namespace FSharpTools
-module Functional = 
+
+module Functional =
+    open FSharpRailway
+    open System
+
+    type Resetter() = 
+        let mutable action: (unit->unit) option = None
+
+        member this.SetResetAction resetAction = 
+            action <- Some resetAction
+
+        member this.Reset () = 
+            if action.IsSome then action.Value()
+        
+    type RefCell<'a> = {
+        mutable Value: 'a option
+        mutable Valid: bool
+    }
+
     /// <summary>
     /// Memoization. A function result is memoized if called for the first time. 
     /// Subsequent calls always returns return the memoized value, not calling the function any more.
@@ -20,4 +38,33 @@ module Functional =
         let memoized = funToMemoize ()
         fun () -> memoized
 
+    /// <summary>
+    /// Memoization. A function result is memoized if called for the first time. 
+    /// Subsequent calls always returns return the memoized value, not calling the function any more.
+    /// </summary>
+    /// <param name="funToMemoize">function with no (unit) input parameter 'a returning 'b</param>
+    /// <param name="resetter">A Resetter to reset the cached value</param>
+    /// <returns>Memoized function with the same signature</returns>
+    let memoizeSingleReset<'a> (funToMemoize: unit->'a) (resetter: Resetter) =
+        let refCell = { 
+            Value = None
+            Valid = false   
+        }
+        resetter.SetResetAction (fun () -> refCell.Valid <- false)
+        let locker = Object()
+        fun () -> 
+            if refCell.Valid then 
+                refCell.Value.Value
+            else
+                lock locker (fun () ->
+                    if refCell.Valid then 
+                        refCell.Value.Value
+                    else 
+                        refCell.Value <- Some <| funToMemoize ()
+                        refCell.Valid <- true
+                        refCell.Value.Value
+                )
+                
+            
+                
     

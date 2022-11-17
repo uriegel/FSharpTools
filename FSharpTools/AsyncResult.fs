b3734144d903c1f5f02691cb22305a3006838841
Uwe Riegel <uriegel@hotmail.de>
2022-11-17 18:43:15 +0100

    XML Comment

--- FSharpTools/AsyncResult.fs
+++ FSharpTools/AsyncResult.fs
@@ -20,17 +20,18 @@ module AsyncResult =
     /// <summary>
     /// Bind operator for composing functions returning Result values (Railway Oriented Programming).
     /// </summary>
-    /// <param name="binder">function with one input parameter 'a returning an Result&lt;'b&gt;</param>
+    /// <param name="binder">function with one input parameter 'a returning an Result&lt;'c, 'b&gt;</param>
     /// <param name="x">input parameter 'a</param>
     /// <returns>Result&lt;'b&gt;</returns>
     let (>>=) x binder = bind binder x
 
     /// <summary>
-    /// Maps the Ok value by  calling function f, leaving the Err value
+    /// Maps the Ok value by  calling function f, leaving the Err value.
     /// Asynchronous version
-    /// <param name="f">function with one input parameter 'a returning 'b</param>
-    /// <param name="x">input parameter Result&lt;'a&gt;</param>
-    /// <returns>Result&lt;'b&gt;</returns>
+    /// </summary>
+    /// <param name="f">function with one input parameter 'a returning Async&lt;'b&gt;</param>
+    /// <param name="x">input parameter Async&lt;Result&lt;'a, 'c&gt;&gt;</param>
+    /// <returns>Async&lt;Result&lt;'b, 'c&gt;&gt;</returns>
     let map f x = async {
         match! x with
         | Ok y ->

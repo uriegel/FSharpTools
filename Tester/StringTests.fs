41ecc707f17e8c5a9d2a2583d41a057b4ee5a738
Uwe Riegel <uriegel@hotmail.de>
2022-11-17 09:45:27 +0100

    Error found while running unit tests

--- Tester/StringTests.fs
+++ Tester/StringTests.fs
@@ -1,20 +1,27 @@
 module Tests
 
-open NUnit.Framework
+open Xunit
+open Xunit.Abstractions
+
 open FSharpTools
 
-[<TestFixture>]
-type StringTest () = 
-    [<Test>]
-    member this.``My test`` () =
-        Assert.True(true)
+type ClassDataBase(generator : obj [] seq) = 
+    interface seq<obj []> with
+        member this.GetEnumerator() = generator.GetEnumerator()
+        member this.GetEnumerator() = 
+            generator.GetEnumerator() :> System.Collections.IEnumerator
 
-    [<Test>]
-    [<TestCase("Start", "End", "A substring between the string Start<here is the content of the substring>End. This part is not to be considered.", ExpectedResult = "<here is the content of the substring>")>]
-    [<TestCase("<Tag>", "</Tag>", "<Doc><Item><Tag>A substring between tags</Tag></Item></Doc", ExpectedResult = "A substring between tags")>]
-    [<TestCase("<start>", "<end>", "In this substring is nothing to be found", ExpectedResult = null)>]
-    member this.``subStringBetweenStrs`` (startStr, endStr, str) =
-        match String.subStringBetweenStrs startStr endStr str with
-        | Some value -> value
-        | None -> null
-    
+type subStringBetweenStrsTest () = 
+    inherit ClassDataBase([ 
+        [| "Start"; "End"; "A substring between the string Start<here is the content of the substring>End. This part is not to be considered."; Some "<here is the content of the substring>" |]
+        [| "<Tag>"; "</Tag>"; "<Doc><Item><Tag>A substring between tags</Tag></Item></Doc"; Some "A substring between tags" |]
+        [| "<start>"; "<end>"; "In this substring is nothing to be found"; None |]
+        [| ""; ""; "In this substring is nothing to be found"; None |]
+        [| "nothing "; ""; "In this substring is nothing to be found"; None |]
+    ])
+
+type StringTests(output: ITestOutputHelper) =
+    [<Theory>]
+    [<ClassData(typeof<subStringBetweenStrsTest>)>]
+    let subStringBetweenStrs (startStr, endStr, str, result) = 
+        Assert.Equal(String.subStringBetweenStrs startStr endStr str, result)

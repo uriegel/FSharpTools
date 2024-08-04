5f0ca8a5ab3977e00bb9c29c8ffb66d7352db5e0
Uwe Riegel <uriegel@hotmail.de>
2024-08-04 18:16:54 +0200

    Test requests

---
+++ Requests/Client.fs
@@ -0,0 +1,25 @@
+﻿namespace FSharpTools.Requests
+open System
+open System.Net.Http
+open FSharpTools.Functional
+
+module Client =
+    let mutable private maxConnectionsVal = 8
+    let mutable private timeoutVal = TimeSpan.FromSeconds 100
+
+    let init maxConnections =
+        maxConnectionsVal <- maxConnections
+
+    let initWithTimeout maxConnections timeout =
+        maxConnectionsVal <- maxConnections
+        timeoutVal <- timeout
+
+
+    let getClient = 
+        let getClient () =
+            new HttpClient(new HttpClientHandler(
+                MaxConnectionsPerServer = maxConnectionsVal),
+                Timeout = timeoutVal)
+        memoizeSingle getClient  
+
+
